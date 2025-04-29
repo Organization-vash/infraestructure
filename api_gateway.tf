7a3c8b5767ec47d8ad2ac6b09edb83d313a6c696
Hernan Mauriccio Berrospi Reyes <mauriccioreyes@gmail.com>
2025-04-29 01:27:00 -0500

    refractor lambda_users, outputs, api_gateway

--- api_gateway.tf
+++ api_gateway.tf
@@ -3,31 +3,31 @@ resource "aws_apigatewayv2_api" "main_api" {
   protocol_type = "HTTP"
 }
 
-#resource "aws_apigatewayv2_route" "user_route" {
-#  api_id    = aws_apigatewayv2_api.main_api.id
-#  route_key = "ANY /users"
-#  target    = "integrations/${aws_apigatewayv2_integration.user_integration.id}"
-#}
+resource "aws_apigatewayv2_route" "user_route" {
+  api_id    = aws_apigatewayv2_api.main_api.id
+  route_key = "ANY /users"
+  target    = "integrations/${aws_apigatewayv2_integration.user_integration.id}"
+}
 
-#resource "aws_apigatewayv2_integration" "user_integration" {
-#  api_id                 = aws_apigatewayv2_api.main_api.id
-#  integration_type       = "AWS_PROXY"
-#  integration_uri        = aws_lambda_function.user_lambda.invoke_arn
-#  integration_method     = "POST"
-#  payload_format_version = "2.0"
-#}
+resource "aws_apigatewayv2_integration" "user_integration" {
+  api_id                 = aws_apigatewayv2_api.main_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.user_lambda.invoke_arn
+  integration_method     = "POST"
+  payload_format_version = "2.0"
+}
 
 
-#resource "aws_lambda_permission" "allow_apigw_invoke_user" {
-#  statement_id  = "AllowExecutionFromAPIGatewayUser"
-#  action        = "lambda:InvokeFunction"
-#  function_name = aws_lambda_function.user_lambda.function_name
-#  principal     = "apigateway.amazonaws.com"
-#  source_arn    = "${aws_apigatewayv2_api.main_api.execution_arn}/*/*"
-#}
+resource "aws_lambda_permission" "allow_apigw_invoke_user" {
+  statement_id  = "AllowExecutionFromAPIGatewayUser"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.user_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.main_api.execution_arn}/*/*"
+}
 
-#resource "aws_apigatewayv2_stage" "default_stage" {
-#  api_id      = aws_apigatewayv2_api.main_api.id
-#  name        = "$default"
-#  auto_deploy = true
-#}
+resource "aws_apigatewayv2_stage" "default_stage" {
+  api_id      = aws_apigatewayv2_api.main_api.id
+  name        = "$default"
+  auto_deploy = true
+}
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-#output "api_gateway_url" {
- # description = "URL pública del API Gateway principal"
- # value       = aws_apigatewayv2_api.main_api.api_endpoint
-#}
+output "api_gateway_url" {
+  description = "URL pública del API Gateway principal"
+  value       = aws_apigatewayv2_api.main_api.api_endpoint
+}
 
-#output "user_lambda_endpoint" {
- # description = "URL completa del endpoint de la Lambda User"
-  #value       = "${aws_apigatewayv2_api.main_api.api_endpoint}/users"
-#}
+output "user_lambda_endpoint" {
+  description = "URL completa del endpoint de la Lambda User"
+  value       = "${aws_apigatewayv2_api.main_api.api_endpoint}/users"
+}
