4170b0deb65a8e60f2c42cfbfe2283c6b4fbe3cf
Sergio Velásquez <svelasquezr1@upao.edu.pe>
2025-06-12 17:43:06 -0500

    feat(s3): habilitar notificaciones EventBridge en lambda_bucket para CKV2_AWS_62

--- iac/s3_lambdas.tf
+++ iac/s3_lambdas.tf
@@ -7,3 +7,10 @@ resource "aws_s3_bucket" "lambda_bucket" {
     Environment = "dev"
   }
 }
+
+resource "aws_s3_bucket_notification" "lambda_eventbridge" {
+  bucket      = aws_s3_bucket.lambda_bucket.id
+  eventbridge = true
+
+  depends_on = [aws_s3_bucket.lambda_bucket]
+}
