7491395ac3c12309336ba3d2eee3c55bc9dce530
Sergio Velásquez <svelasquezr1@upao.edu.pe>
2025-06-12 18:02:49 -0500

    feat(s3): agregar Public Access Block al bucket lambda_bucket para CKV2_AWS_6

--- iac/s3_lambdas.tf
+++ iac/s3_lambdas.tf
@@ -8,6 +8,15 @@ resource "aws_s3_bucket" "lambda_bucket" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "lambda_block" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_notification" "lambda_eventbridge" {
   bucket      = aws_s3_bucket.lambda_bucket.id
   eventbridge = true
