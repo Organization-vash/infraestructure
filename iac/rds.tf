5ee8b9e07519d652de3eb19f1bc69514e04de2f5
StefanoYZ <ayepezz1@upao.edu.pe>
2025-06-13 00:31:19 -0500

    [CKV_AWS_353] Activado Performance Insights en instancia RDS PostgreSQL para análisis de rendimiento

--- iac/rds.tf
+++ iac/rds.tf
@@ -45,6 +45,7 @@ resource "aws_db_instance" "postgres" {
    auto_minor_version_upgrade = true
    iam_database_authentication_enabled = true
    storage_encrypted                      = true
+  performance_insights_enabled = true
 
   deletion_protection = true
   parameter_group_name         = aws_db_parameter_group.postgres_logging.name
