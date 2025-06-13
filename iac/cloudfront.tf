eab61c3f5628ac8d25133272b6c7efd77b002637
StefanoYZ <ayepezz1@upao.edu.pe>
2025-06-12 19:52:43 -0500

    [CKV_AWS_374] (se agrego restriccion geografica en CloudFront)

--- iac/cloudfront.tf
+++ iac/cloudfront.tf
@@ -35,7 +35,8 @@ resource "aws_cloudfront_distribution" "frontend_distribution" {
 
   restrictions {
     geo_restriction {
-      restriction_type = "none"
+      restriction_type = "blacklist"
+      locations        = ["CN", "RU", "KP", "IR"]
     }
   }
 
