019394558c0e138efeed99665732dcb640f09e9c
oscarllc <ollaurec1@upao.edu.pe>
2025-06-13 00:25:10 -0500

    Se resolvio error [CKV_AWS_16] – Ensure all data stored in the RDS is securely encrypted

--- iac/rds.tf
+++ iac/rds.tf
@@ -43,6 +43,7 @@ resource "aws_db_instance" "postgres" {
 
    auto_minor_version_upgrade = true
    iam_database_authentication_enabled = true
+   storage_encrypted                      = true
 
   deletion_protection = true
   parameter_group_name         = aws_db_parameter_group.postgres_logging.name
