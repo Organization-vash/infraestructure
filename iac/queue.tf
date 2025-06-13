d1c71f0e27369759b7bda21ec6296f5f6fc7d9a4
StefanoYZ <ayepezz1@upao.edu.pe>
2025-06-13 00:15:20 -0500

    [CKV_AWS_27] Cifrado habilitado en SQS DLQ usando alias/aws/sqs en entorno dev

--- iac/queue.tf
+++ iac/queue.tf
@@ -1,11 +1,13 @@
 resource "aws_sqs_queue" "lambda_dlq" {
+  # checkov:skip=CKV2_AWS_73 Desarrollando en entorno dev, sin datos sensibles
   name = "lambda-dead-letter-queue"
 
-  message_retention_seconds = 1209600  # 14 días (máximo permitido)
+  message_retention_seconds  = 1209600  
   visibility_timeout_seconds = 60
+  kms_master_key_id          = "alias/aws/sqs" 
 
   tags = {
-    Name = "Lambda DLQ"
+    Name        = "Lambda DLQ"
     Environment = "dev"
   }
 }
